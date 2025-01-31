e6a34728521add57725bb83229d749595086c24a
JCanadas2 <j.canadas@zonadesarrollo.com>
2025-01-31 11:55:56 +0100

    seccion 3 terminada

---
+++ Recursos/03-Strings/js/01-app.js
@@ -0,0 +1,7 @@
+const producto = "Monitor 20\""; 
+const producto2 = String('Monitor 24 Pulgadas');
+
+const producto3 = new String('monitor 27 pulgadas');
+
+console.log(producto);
+console.log(producto2);
---
+++ Recursos/03-Strings/js/02-app.js
@@ -0,0 +1,15 @@
+const producto = 'Monitor 20 Pulgadas';
+
+console.log(producto);
+
+
+console.log(produto.length);
+
+
+
+
+
+
+console.log(producto.indexOf('Monitor'));
+console.log(producto.indexOf('Tablet'));
+console.log(producto.indexOf('Monitor'));
